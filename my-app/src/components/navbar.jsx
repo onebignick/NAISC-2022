ef695548a113c3e226166521addf6c86b619b20f
Nick <nicholas.zhengkai.ong@gmail.com>
2023-01-08 17:32:48 +0800

    added search bar

--- my-app/src/components/navbar.jsx
+++ my-app/src/components/navbar.jsx
@@ -1,12 +1,13 @@
 import './styles/css-reset.scss';
 import './styles/navbar.scss';
-
+import SearchIcon from './media/search-icon.svg'
 export default function Navbar () {
     return(
         <div className='navbar-container'>
             <form id="search-bar-form">
                 <input type="text" id="search-bar"></input>
             </form>
+            <img src={SearchIcon}></img>
         </div>
     )
 }
